224f19696d9f011f5f4429f3a9e99d6e02bd966c
Gilberto Stankiewicz <hello@kiewic.com>
2019-05-11 18:49:44 -0700

    Add utils to check DPI of every process

---
+++ CefSharpApp/DpiUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharpApp
+{
+    // Source: https://stackoverflow.com/a/49115675/27211
+    internal enum PROCESS_DPI_AWARENESS
+    {
+        PROCESS_DPI_UNAWARE = 0,
+        PROCESS_SYSTEM_DPI_AWARE = 1,
+        PROCESS_PER_MONITOR_DPI_AWARE = 2
+    }
+
+    internal enum DPI_AWARENESS : int
+    {
+        DPI_AWARENESS_INVALID = -1,
+        DPI_AWARENESS_UNAWARE = 0,
+        DPI_AWARENESS_SYSTEM_AWARE = 1,
+        DPI_AWARENESS_PER_MONITOR_AWARE = 2
+    }
+
+    class DpiUtils
+    {
+        [DllImport("SHcore.dll")]
+        internal static extern int GetProcessDpiAwareness(IntPtr hWnd, out PROCESS_DPI_AWARENESS value);
+
+        [DllImport("user32.dll")]
+        internal static extern uint GetDpiForWindow(IntPtr hWnd);
+
+        // When app.manifest has:
+        //     <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
+        // Values is:
+        //     PROCESS_SYSTEM_DPI_AWARE
+        //
+        // When app.manifest has:
+        //     <dpiAware>true/PM</dpiAware>
+        // Value is:
+        //     PROCESS_PER_MONITOR_DPI_AWARE
+        public static void Check()
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                //if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                if (process.MainWindowHandle != IntPtr.Zero)
+                {
+                    // Per process
+                    PROCESS_DPI_AWARENESS processAwareness;
+                    GetProcessDpiAwareness(process.Handle, out processAwareness);
+
+                    uint windowDpi = GetDpiForWindow(process.MainWindowHandle);
+
+                    Console.WriteLine("Process: {0} ID: {1} Window title: {2} {3} {4}",
+                        process.ProcessName,
+                        process.Id,
+                        process.MainWindowTitle,
+                        processAwareness,
+                        windowDpi);
+                }
+            }
+
+            //PROCESS_DPI_AWARENESS awareness;
+            //GetProcessDpiAwareness(this.Handle, out awareness);
+            Debug.WriteLine("Done");
+        }
+    }
+}
--- CefSharpApp/Form1.cs
+++ CefSharpApp/Form1.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
@@ -17,18 +8,6 @@ namespace CefSharpApp
     public partial class Form1 : Form
     {
         public ChromiumWebBrowser chromeBrowser;
-
-        // From: https://stackoverflow.com/a/49115675/27211
-        internal enum PROCESS_DPI_AWARENESS
-        {
-            PROCESS_DPI_UNAWARE = 0,
-            PROCESS_SYSTEM_DPI_AWARE = 1,
-            PROCESS_PER_MONITOR_DPI_AWARE = 2
-        }
-
-        [DllImport("SHcore.dll")]
-        internal static extern int GetProcessDpiAwareness(IntPtr hWnd, out PROCESS_DPI_AWARENESS value);
-
 
         public Form1()
         {
@@ -44,6 +23,7 @@ namespace CefSharpApp
         public void InitializeChromium()
         {
             CefSettings settings = new CefSettings();
+
             // For Windows 7 and above, best to include relevant app.manifest entries as well
             //Cef.EnableHighDPISupport();
 
@@ -62,10 +42,6 @@ namespace CefSharpApp
             chromeBrowser.JavascriptObjectRepository.Register("coolGuidGenerator", new GuidGenerator());
             chromeBrowser.JavascriptObjectRepository.Register("coolGuidGenerator2", new GuidGenerator(), isAsync: true);
             chromeBrowser.JavascriptObjectRepository.Register("boundAsync", new BoundObject(), true);
-
-            PROCESS_DPI_AWARENESS awareness;
-            GetProcessDpiAwareness(this.Handle, out awareness);
-            Debug.WriteLine(awareness);
         }
 
         private void ChromeBrowser_IsBrowserInitializedChanged(object sender, IsBrowserInitializedChangedEventArgs e)
@@ -78,6 +54,8 @@ namespace CefSharpApp
 
             // DevTools can be called from here too
             // chromeBrowser.ShowDevTools();
+
+            DpiUtils.Check();
         }
     }
 }
