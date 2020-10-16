f818304dd488d86612928cf3035098a89249a5e3
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2020-10-16 02:49:05 -0700

    Remove Form1.Designer.cs and Form.resx

--- CefSharpApp/Form1.cs
+++ CefSharpApp/Form1.cs
@@ -14,8 +14,34 @@ using CefSharp.WinForms;
 
 namespace CefSharpApp
 {
+    [System.ComponentModel.DesignerCategory("")]
     public partial class Form1 : Form
     {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 802);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
         private enum PROCESS_DPI_AWARENESS
         {
             Process_DPI_Unaware = 0,
